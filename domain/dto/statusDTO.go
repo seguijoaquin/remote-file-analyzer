9a15a629751efa6cf1050bdf32d38a0639855ca3
Joaquin Segui <joaquin.segui@mercadolibre.com>
2019-09-30 21:02:12 -0300

    Update project structure

--- domain/dto/statusDTO.go
+++
@@ -1,9 +0,0 @@
-package dto
-
-// StatusDTO represents the messages the StatusController sends/receives
-type StatusDTO struct {
-	Host   string `json:"host"`
-	Path   string `json:"path"`
-	Status string `json:"status"`
-	Data   []byte `json:"data"`
-}
