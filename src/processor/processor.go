44a4e8027c5e91c4b9f78d8e53a42ed3a9ed33cc
Joaquin Segui <joaquin.segui@mercadolibre.com>
2019-10-28 21:55:25 -0300

    Processor handles listSubdir requests through internal channel & introduces ftp per client mutex lock

--- src/processor/processor.go
+++ src/processor/processor.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net"
 	"os"
 	"path/filepath"
@@ -11,40 +12,6 @@ import (
 	"strings"
 	"sync"
 )
-
-type ftpMetadata struct {
-	fileName string
-	fileSize string
-	isDir    bool
-}
-
-type connectionsPool struct {
-	container map[string]*net.Conn
-	mu        *sync.Mutex
-}
-
-type ftpWorker struct {
-	id        int
-	pListener *net.Listener
-	wg        *sync.WaitGroup
-	ftpConns  *connectionsPool
-}
-
-func newConnectionsPool() *connectionsPool {
-	return &connectionsPool{
-		container: make(map[string]*net.Conn),
-		mu:        &sync.Mutex{},
-	}
-}
-
-func newFtpWorker(id int, pListener *net.Listener, wg *sync.WaitGroup, ftpConns *connectionsPool) *ftpWorker {
-	return &ftpWorker{
-		id:        id,
-		pListener: pListener,
-		wg:        wg,
-		ftpConns:  ftpConns,
-	}
-}
 
 // Send is responsible for sending messages through a
 // generic stream-oriented network connection
@@ -62,6 +29,47 @@ func send(id int, conn *net.Conn, request []byte) error {
 		}
 	}
 	return err
+}
+
+type ftpMetadata struct {
+	fileName string
+	fileSize string
+	isDir    bool
+}
+
+type ftpConnection struct {
+	conn *net.Conn
+	mu   *sync.Mutex
+}
+
+type connectionsPool struct {
+	container map[string]*ftpConnection
+	mu        *sync.Mutex
+}
+
+type ftpWorker struct {
+	id           int
+	wg           *sync.WaitGroup
+	ftpConns     *connectionsPool
+	connsChan    chan net.Conn
+	requestsChan chan processorRequestDTO
+}
+
+func newConnectionsPool() *connectionsPool {
+	return &connectionsPool{
+		container: make(map[string]*ftpConnection),
+		mu:        &sync.Mutex{},
+	}
+}
+
+func newFtpWorker(id int, wg *sync.WaitGroup, ftpConns *connectionsPool, connsChan chan net.Conn, reqChan chan processorRequestDTO) *ftpWorker {
+	return &ftpWorker{
+		id:           id,
+		wg:           wg,
+		ftpConns:     ftpConns,
+		connsChan:    connsChan,
+		requestsChan: reqChan,
+	}
 }
 
 func (w *ftpWorker) sendCommand(conn *net.Conn, request []byte) error {
@@ -135,19 +143,33 @@ func (w *ftpWorker) login(processorRequest processorRequestDTO) (*net.Conn, erro
 func (w *ftpWorker) getConnection(processorRequest processorRequestDTO) (conn *net.Conn, err error) {
 	id := processorRequest.Host
 	if _, ok := w.ftpConns.container[id]; ok {
+		w.ftpConns.container[id].mu.Lock()
 		fmt.Printf("[worker: %d] [CLIENT] Found connection in container Pool for: %s\n", w.id, id)
-		return w.ftpConns.container[id], nil
+		return w.ftpConns.container[id].conn, nil
 	}
 
+	return nil, fmt.Errorf("Connection not found for host: %s", processorRequest.Host)
+
+	// w.ftpConns.mu.Lock()
+	// defer w.ftpConns.mu.Unlock()
+	// conn, err = w.login(processorRequest)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// w.ftpConns.container[id] = &ftpConnection{conn: conn, mu: &sync.Mutex{}}
+
+	// return conn, nil
+}
+
+func (w *ftpWorker) releaseConnection(processorRequest processorRequestDTO) {
+	w.ftpConns.container[processorRequest.Host].mu.Unlock()
+}
+
+func (w *ftpWorker) saveConnection(conn *net.Conn, processorRequest processorRequestDTO) {
+	id := processorRequest.Host
 	w.ftpConns.mu.Lock()
 	defer w.ftpConns.mu.Unlock()
-	conn, err = w.login(processorRequest)
-	if err != nil {
-		return nil, err
-	}
-	w.ftpConns.container[id] = conn
-
-	return conn, nil
+	w.ftpConns.container[id] = &ftpConnection{conn: conn, mu: &sync.Mutex{}}
 }
 
 func (w *ftpWorker) store(request processorRequestDTO, fileName string, sFileSize string, isDir bool) error {
@@ -201,166 +223,130 @@ func (w *ftpWorker) updateStatus(request processorRequestDTO, fileName string, i
 	return send(w.id, &statusConn, statusRequest)
 }
 
-func (w *ftpWorker) listSubdir(request processorRequestDTO, fileName string) error {
-	processorConn, err := net.Dial("tcp", setup.getProcessorHost())
-	if err != nil {
-		return err
-	}
-	defer processorConn.Close()
-
-	processorRequest, err := json.Marshal(processorRequestDTO{
+func (w *ftpWorker) listSubdir(request processorRequestDTO, fileName string) {
+	w.requestsChan <- processorRequestDTO{
 		Host:   request.Host,
 		Path:   filepath.Join(request.Path, fileName),
 		Action: "LIST",
-	})
-	if err != nil {
-		return err
 	}
-
-	return send(w.id, &processorConn, processorRequest)
 }
 
-func (w *ftpWorker) handleRunError(err error, conn *net.Conn) bool {
+// func (w *ftpWorker) handleRunError(err error, conn *net.Conn) bool {
+// 	if err != nil {
+// 		fmt.Fprintf(os.Stderr, "[worker: %d] [CLIENT] Fatal error: %s\n", w.id, err.Error())
+// 		if err := w.reply(conn, "", err.Error()); err != nil {
+// 			fmt.Fprintf(os.Stderr, "[worker: %d] [CLIENT] Fatal error: %s\n", w.id, err.Error())
+// 		}
+// 		return true
+// 	}
+// 	return false
+// }
+
+func (w *ftpWorker) failOnError(err error) {
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "[worker: %d] [CLIENT] Fatal error: %s\n", w.id, err.Error())
-		if err := w.reply(conn, "", err.Error()); err != nil {
-			fmt.Fprintf(os.Stderr, "[worker: %d] [CLIENT] Fatal error: %s\n", w.id, err.Error())
-		}
-		return true
+		log.Fatalf("[worker: %d] Fatal error: %s\n", w.id, err.Error())
 	}
-	return false
+}
+
+func (w *ftpWorker) processRequest(processorRequest processorRequestDTO) {
+	// Send PASV command and check response
+	ftpConn, err := w.getConnection(processorRequest)
+	w.failOnError(err)
+	connReader := bufio.NewReader(*ftpConn)
+	w.failOnError(w.sendCommand(ftpConn, []byte("PASV\n")))
+
+	message, err := connReader.ReadString('\n')
+	w.failOnError(err)
+	w.failOnError(w.checkResponse(message, "227"))
+
+	// Scan PASV response and build HOST:PORT to FTP Data Conn
+	var portPref, portSuff, port, ip1, ip2, ip3, ip4 int
+	_, err = fmt.Sscanf(message, "227 Entering Passive Mode (%d,%d,%d,%d,%d,%d).",
+		&ip1, &ip2, &ip3, &ip4, &portPref, &portSuff)
+	w.failOnError(err)
+	port = portPref*256 + portSuff
+	dataHost := processorRequest.Host + ":" + fmt.Sprintf("%d", port)
+
+	// Make FTP Data Conn
+	dataConn, err := net.Dial("tcp", dataHost)
+	w.failOnError(err)
+
+	// Send LIST command and check response
+	w.failOnError(w.sendCommand(ftpConn, []byte(fmt.Sprintf("LIST %s\n", processorRequest.Path))))
+	message, err = connReader.ReadString('\n')
+	w.failOnError(err)
+	w.failOnError(w.checkResponse(message, "150"))
+
+	// Check for finished Listing
+	message, err = connReader.ReadString('\n')
+	w.failOnError(err)
+	w.failOnError(w.checkResponse(message, "226"))
+
+	w.releaseConnection(processorRequest)
+
+	dconnbuf := bufio.NewScanner(dataConn)
+
+	var files []ftpMetadata
+
+	// Parse response from FTP Data Conn and
+	// send response to Status Controller, Persistor and Storage
+	for dconnbuf.Scan() {
+		buff := dconnbuf.Text()
+		fields := strings.Fields(buff)
+		files = append(files, ftpMetadata{
+			fileName: fields[8],
+			fileSize: fields[4],
+			isDir:    strings.Contains(fields[0], "d"),
+		})
+	}
+
+	w.failOnError(dconnbuf.Err())
+
+	for _, val := range files {
+		if err := w.store(processorRequest, val.fileName, val.fileSize, val.isDir); err != nil {
+			fmt.Fprintf(os.Stderr, "[worker: %d] Fatal error: %s\n", w.id, err.Error())
+			break
+		}
+		if err := w.updateStatus(processorRequest, val.fileName, val.isDir); err != nil {
+			fmt.Fprintf(os.Stderr, "[worker: %d] Fatal error: %s\n", w.id, err.Error())
+			break
+		}
+		if val.isDir && val.fileName != "sys" {
+			w.listSubdir(processorRequest, val.fileName)
+		}
+	}
 }
 
 func (w *ftpWorker) run() {
 	defer w.wg.Done()
 
 	for {
-		func() {
-			// Waits on connections from Daemon or FTP Processor
-			processorConn, err := (*w.pListener).Accept()
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "[worker: %d] Fatal error: %s\n", w.id, err.Error())
-				return
-			}
-			defer processorConn.Close()
+		select {
+		case processorConn := <-w.connsChan:
 
-			// Decodes new incoming message from Daemon or FTP Processor
+			// Decodes new incoming connection from channel
 			var processorRequest processorRequestDTO
-			if err = json.NewDecoder(processorConn).Decode(&processorRequest); err != nil {
-				fmt.Fprintf(os.Stderr, "[worker: %d] Fatal error: %s\n", w.id, err.Error())
-				return
-			}
+			w.failOnError(json.NewDecoder(processorConn).Decode(&processorRequest))
 
 			fmt.Printf("[worker: %d] [host: %s] [user: %v] [path: %s]\n",
 				w.id, processorRequest.Host, processorRequest.User, processorRequest.Path)
 
-			// We get the FTP server connection and it's reader
-			ftpConn, err := w.getConnection(processorRequest)
-			if w.handleRunError(err, &processorConn) {
-				return
-			}
-			connReader := bufio.NewReader(*ftpConn)
-
 			// If its just a login, we return early
 			if processorRequest.Action == "LOGIN" {
-				if err := w.reply(&processorConn, "OK", ""); err != nil {
-					w.handleRunError(err, &processorConn)
-				}
+				ftpConn, _ := w.login(processorRequest)
+				w.saveConnection(ftpConn, processorRequest)
+				w.reply(&processorConn, "OK", "")
 				return
 			}
 
-			// Send PASV command and check response
-			w.sendCommand(ftpConn, []byte("PASV\n"))
-			message, err := connReader.ReadString('\n')
-			if w.handleRunError(err, &processorConn) {
-				return
-			}
-			err = w.checkResponse(message, "227")
-			if w.handleRunError(err, &processorConn) {
-				return
-			}
+			w.requestsChan <- processorRequest
 
-			// Scan PASV response and build HOST:PORT to FTP Data Conn
-			var portPref, portSuff, port, ip1, ip2, ip3, ip4 int
-			_, err = fmt.Sscanf(message, "227 Entering Passive Mode (%d,%d,%d,%d,%d,%d).",
-				&ip1, &ip2, &ip3, &ip4, &portPref, &portSuff)
-			if w.handleRunError(err, &processorConn) {
-				return
-			}
-			port = portPref*256 + portSuff
-			dataHost := processorRequest.Host + ":" + fmt.Sprintf("%d", port)
+			w.reply(&processorConn, "OK", "")
 
-			// Make FTP Data Conn
-			dataConn, err := net.Dial("tcp", dataHost)
-			if w.handleRunError(err, &processorConn) {
-				return
-			}
-			defer dataConn.Close()
-
-			// Send LIST command and check response
-			w.sendCommand(ftpConn, []byte(fmt.Sprintf("LIST %s\n", processorRequest.Path)))
-			message, err = connReader.ReadString('\n')
-			if w.handleRunError(err, &processorConn) {
-				return
-			}
-			err = w.checkResponse(message, "150")
-			if w.handleRunError(err, &processorConn) {
-				return
-			}
-
-			// Check for finished Listing
-			message, err = connReader.ReadString('\n')
-			if w.handleRunError(err, &processorConn) {
-				return
-			}
-			err = w.checkResponse(message, "226")
-			if w.handleRunError(err, &processorConn) {
-				return
-			}
-
-			dconnbuf := bufio.NewScanner(dataConn)
-
-			var files []ftpMetadata
-
-			// Parse response from FTP Data Conn and
-			// send response to Status Controller, Persistor and Storage
-			for dconnbuf.Scan() {
-				buff := dconnbuf.Text()
-				fields := strings.Fields(buff)
-				files = append(files, ftpMetadata{
-					fileName: fields[8],
-					fileSize: fields[4],
-					isDir:    strings.Contains(fields[0], "d"),
-				})
-				//fmt.Printf("[worker: %d] %s\n", w.id, buff)
-			}
-			if err := dconnbuf.Err(); err != nil {
-				fmt.Fprintf(os.Stderr, "[worker: %d] Fatal error: %s\n", w.id, err.Error())
-				return
-			}
-
-			for _, val := range files {
-				if err := w.store(processorRequest, val.fileName, val.fileSize, val.isDir); err != nil {
-					fmt.Fprintf(os.Stderr, "[worker: %d] Fatal error: %s\n", w.id, err.Error())
-					break
-				}
-				if err := w.updateStatus(processorRequest, val.fileName, val.isDir); err != nil {
-					fmt.Fprintf(os.Stderr, "[worker: %d] Fatal error: %s\n", w.id, err.Error())
-					break
-				}
-				if val.isDir {
-					if err := w.listSubdir(processorRequest, val.fileName); err != nil {
-						fmt.Fprintf(os.Stderr, "[worker: %d] Fatal error: %s\n", w.id, err.Error())
-						break
-					}
-				}
-			}
-
-			// Tell Daemon or Processor it's OK
-			if err := w.reply(&processorConn, "OK", ""); err != nil {
-				fmt.Fprintf(os.Stderr, "[worker: %d] Fatal error: %s\n", w.id, err.Error())
-			}
-		}()
+			processorConn.Close()
+		case request := <-w.requestsChan:
+			w.processRequest(request)
+		}
 	}
 }
 
@@ -379,11 +365,22 @@ func main() {
 	defer listener.Close()
 
 	ftpConnections := newConnectionsPool()
+	processorConnectionsChan := make(chan net.Conn, 1000)
+	processorRequestsChan := make(chan processorRequestDTO, 10000)
 
 	for w := 1; w <= setup.getWorkersProcessor(); w++ {
 		workersWg.Add(1)
-		worker := newFtpWorker(w, &listener, &workersWg, ftpConnections)
-		go worker.run()
+		processorWorker := newFtpWorker(w, &workersWg, ftpConnections, processorConnectionsChan, processorRequestsChan)
+		go processorWorker.run()
+	}
+
+	for {
+		processorConn, err := (listener).Accept()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Fatal error: %s\n", err.Error())
+			continue
+		}
+		processorConnectionsChan <- processorConn
 	}
 
 	workersWg.Wait()
